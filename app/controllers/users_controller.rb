8b9fb729ee60acce370e6fb031c9f64c70bb28b5
eric <eric.razorjr@gmail.com>
2021-05-01 14:39:31 -0400

    Add query params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,5 @@
 class UsersController < ApplicationController
     def create
-        query_params = {
-            client_id: ENV["CLIENT_ID"],
-            response_type: "code",
-            redirect_uri: ENV["REDIRECT_URI"],
-            scope: 'user-read-playback-position user-read-playback-state user-library-read',
-            show_dialog: true
-        }
-        url = 'https://accounts.spotify.com/authorize'
-
+        byebug
     end
 end
