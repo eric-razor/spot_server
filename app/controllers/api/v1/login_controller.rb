e68d13beb442365a4d200435e471e0fcba5c179f
eric <eric.razorjr@gmail.com>
2021-05-11 09:29:48 -0400

    Remove bad code

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -2,8 +2,6 @@ class Api::V1::LoginController < ApplicationController
     require "Base64"
     require "net/http"
     def create
-        debugger
-
         query_params = {
             client_id: ENV["CLIENT_ID"],
             response_type: "code",
@@ -14,22 +12,6 @@ class Api::V1::LoginController < ApplicationController
         url = 'https://accounts.spotify.com/authorize/'
 
         redirect_to "#{url}?#{query_params.to_query}"
-    end
-
-    def token
-        uri = 'https://accounts.spotify.com/api/token'
-        encoded = Base64::encode64("#{ENV['CLIENT_ID']}:#{ENV['CLIENT_SECRET']}").gsub(/\n/,"")
-
-        query_params = {
-            code: params[:code],
-            redirect_uri: params[:redirect_uri],
-            grant_type: "authorization_code",
-            headers: {
-                Authorization: encoded,
-            }
-        }
-
-        # res = Net::HTTP.post_form(uri, 'q' => ["#{query_params}"])
     end
 
 end
