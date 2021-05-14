a9544e71929a7b8d5c68d49b7b0f427a33185caa
eric <eric.razorjr@gmail.com>
2021-05-13 22:22:36 -0400

    Fix namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/login', to: 'users#create'
-  resources :users
+  
+  namespace :api do
+    namespace :v1 do 
+      get '/login', to: 'login#create'
+      post '/token', to: 'user#create'
+      
+      
+      resources :users
+
+    end
+  end
 
 end
