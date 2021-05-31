39a5ebefd09ee9a50d8a59327bfef8c4c3210f9f
eric <eric.razorjr@gmail.com>
2021-05-31 19:29:27 -0400

    Change header value

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,34 +1,39 @@
 class Api::V1::UserController < ApplicationController
     require "Base64"
     require "net/http"
+    
     def create
         uri = URI.parse('https://accounts.spotify.com/api/token')
         encoded = Base64::strict_encode64("#{ENV["CLIENT_ID"]}:#{ENV["CLIENT_SECRET"]}")
 
-        form_params = {
+        body = {
             code: params[:code],
             redirect_uri: params[:redirect_uri],
             grant_type: "authorization_code",
         }
 
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = (uri.scheme == "https")
-        req = Net::HTTP::Post.new(uri.request_uri)
-        req.set_form_data(form_params)
-        req["Authorization"] = 'Authorization: Basic ' +  Base64::strict_encode64(ENV["CLIENT_ID"] + ":" + ENV["CLIENT_SECRET"])
+        header = {
+            Authorization: 'Authorization: Basic ' +  encoded
+        }
+    
 
-        res = http.request(req)
+        https = Net::HTTP.new(uri.host, uri.port)
+        https.use_ssl = (uri.scheme == "https")
+        req = Net::HTTP::Post.new(uri.request_uri)
+        
+        req.set_form_data(body, header)
+        
+        res = https.request(req)
         res.inspect
 
         debugger
 
         case res
         when Net::HTTPSuccess, Net::HTTPRedirection
-
+            
         else
-        res.value
+            res.value
         end
-
 
     end
 end
