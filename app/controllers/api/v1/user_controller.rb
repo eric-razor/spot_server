ff72e22b023b7ba557edcc1f5bebd4bf1b596843
eric <eric.razorjr@gmail.com>
2021-06-13 15:47:16 -0400

    Refactor HTTP request

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,39 +1,19 @@
 class Api::V1::UserController < ApplicationController
-    require "Base64"
-    require "net/http"
     
     def create
-        uri = URI.parse('https://accounts.spotify.com/api/token')
-        encoded = Base64::strict_encode64("#{ENV["CLIENT_ID"]}:#{ENV["CLIENT_SECRET"]}")
+        uri = URI('https://accounts.spotify.com/api/token')
 
         body = {
             code: params[:code],
             redirect_uri: params[:redirect_uri],
             grant_type: "authorization_code",
+            client_id: ENV["CLIENT_ID"],
+            client_secret: ENV["CLIENT_SECRET"],
         }
 
-        header = {
-            Authorization: 'Authorization: Basic ' +  encoded
-        }
-    
+        resp = HTTParty.post(uri, body: body)
 
-        https = Net::HTTP.new(uri.host, uri.port)
-        https.use_ssl = (uri.scheme == "https")
-        req = Net::HTTP::Post.new(uri.request_uri)
-        
-        req.set_form_data(body, header)
-        
-        res = https.request(req)
-        res.inspect
-
-        debugger
-
-        case res
-        when Net::HTTPSuccess, Net::HTTPRedirection
-            
-        else
-            res.value
-        end
-
+        render json: resp
     end
+
 end
