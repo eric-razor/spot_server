9cc4e109ef5f9e51da9e46f11c126772e40a7fdf
eric <eric.razorjr@gmail.com>
2021-06-27 21:27:28 -0400

    Remove create action

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -2,16 +2,7 @@ class Api::V1::LoginController < ApplicationController
     require "Base64"
     require "net/http"
     def create
-        query_params = {
-            client_id: ENV["CLIENT_ID"],
-            response_type: "code",
-            redirect_uri: ENV["REDIRECT_URI"],
-            scope: "user-read-playback-position user-read-playback-state user-library-read",
-            show_dialog: true,
-        }
-        url = 'https://accounts.spotify.com/authorize/'
-
-        redirect_to "#{url}?#{query_params.to_query}"
+    
     end
 
 end
