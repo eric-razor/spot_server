2e6a084d6f975e08e324d0f59139f9f58d07ee0c
eric <eric.razorjr@gmail.com>
2021-07-01 15:37:45 -0400

    Include additional fields

--- db/migrate/20210411191341_create_users.rb
+++ db/migrate/20210411191341_create_users.rb
@@ -1,8 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :account
-      t.string :name
+      t.string :username
+      t.string :access_token
+      t.string :refresh_token
+      t.string :spotify_url
+      t.string :profile_picture
+      t.string :href
+      t.string :uri
 
       t.timestamps
     end
