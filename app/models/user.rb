a68f06dd472bd5bfbc6e60efebd8a974b9d9b33e
eric <eric.razorjr@gmail.com>
2021-07-06 20:49:17 -0400

    Request refresh token method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
 class User < ApplicationRecord
     has_many :songs
+
+    validates :username, uniqueness: true, presence: true
+
+    def access_token_expired?
+        (Time.now - self.updated_at) > 3300
+    end
+
+    def refresh_access_token
+        if access_token_expired?
+            
+            body = {
+                grant_type: "refresh_token",
+                refresh_token: self.refresh_token,
+                client_id: ENV["CLIENT_ID"],
+                client_secret: ENV["CLIENT_SECRET"]
+            }
+
+            refresh_response = HTTParty.post('https://accounts.spotify.com/api/token', body)
+            refresh_params = JSON.parse(refresh_response)
+            self.update(access_token: refresh_params["access_token"])
+        else
+            puts "Token not expired"
+        end
+    end
 end
+
